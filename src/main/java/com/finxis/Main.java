7d7b1720d572f0d284812337fdc31ebe31ba223f
tomhealey-icma <tom.healey@icmagroup.org>
2025-03-18 14:38:06 -0400

    added basic bond example

--- src/main/java/com/finxis/Main.java
+++ src/main/java/com/finxis/Main.java
@@ -1,23 +1,40 @@
 package com.finxis;
 
 import cdm.base.datetime.AdjustableDate;
+import cdm.base.datetime.AdjustableOrAdjustedOrRelativeDate;
 import cdm.base.datetime.AdjustableOrRelativeDate;
 import cdm.base.datetime.TimeZone;
 import cdm.base.datetime.daycount.DayCountFractionEnum;
 import cdm.base.datetime.daycount.functions.DayCountBasis;
 import cdm.base.datetime.daycount.metafields.FieldWithMetaDayCountFractionEnum;
 import cdm.base.datetime.metafields.FieldWithMetaTimeZone;
+import cdm.base.math.ArithmeticOperationEnum;
+import cdm.base.math.NonNegativeQuantitySchedule;
 import cdm.base.math.UnitType;
+import cdm.base.math.metafields.FieldWithMetaNonNegativeQuantitySchedule;
 import cdm.base.staticdata.asset.common.*;
+import cdm.base.staticdata.identifier.AssignedIdentifier;
 import cdm.base.staticdata.identifier.Identifier;
+import cdm.base.staticdata.identifier.TradeIdentifierTypeEnum;
+import cdm.base.staticdata.party.*;
+import cdm.base.staticdata.party.metafields.ReferenceWithMetaParty;
+import cdm.event.common.ExecutionDetails;
 import cdm.event.common.Trade;
+import cdm.event.common.TradeIdentifier;
 import cdm.observable.asset.*;
+import cdm.observable.asset.metafields.FieldWithMetaObservable;
+import cdm.observable.asset.metafields.FieldWithMetaPriceSchedule;
 import cdm.observable.asset.metafields.ReferenceWithMetaPriceSchedule;
 import cdm.product.asset.FixedRateSpecification;
 import cdm.product.asset.InterestRatePayout;
 import cdm.product.asset.RateSpecification;
 import cdm.product.common.schedule.RateSchedule;
+import cdm.product.common.settlement.SettlementDate;
+import cdm.product.common.settlement.SettlementTerms;
 import cdm.product.template.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.regnosys.rosetta.common.serialisation.RosettaObjectMapper;
 import com.rosetta.model.lib.records.Date;
 import com.rosetta.model.metafields.FieldWithMetaDate;
 import com.rosetta.model.metafields.FieldWithMetaString;
@@ -30,7 +47,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException {
         System.out.println("CDM Bond Demo");
 
         Main main = new Main();
@@ -38,30 +55,49 @@ public class Main {
         Product product = main.createBondProduct(bondModel);
         Trade trade = main.createTrade(product, bondModel);
 
+        String tradeJson = RosettaObjectMapper.getNewRosettaObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(trade);
+        System.out.println(tradeJson);
+
+        ObjectMapper rosettaObjectMapper = RosettaObjectMapper.getNewRosettaObjectMapper();
+        Trade tradeObj = new Trade.TradeBuilderImpl();
+        Trade newTrade = rosettaObjectMapper.readValue(tradeJson, tradeObj.getClass());
+
     }
 
 public BondModel setBondData(){
 
         BondModel bondModel = new BondModel();
 
-        bondModel.maturityDate = "2026-04-09";
+        bondModel.maturityDate = "2026-04-09T00:00:00.000+00:00";
         bondModel.couponRate = "2.375";
         bondModel.country = "D";
 
-        bondModel.tradeDate = "2025-02-27";
+        bondModel.tradeDate = "2025-02-27T15:38:16.000+00:00";
         bondModel.tradeTime = "153816";
         bondModel.price = "99.80000000";
         bondModel.nominalQuantity = "500000.00000000";
 
-        return setBondData();
+        bondModel.commission = "20.45";
+        bondModel.settlementDate = "2025-03-03T00:00:00.000+00:00";
+        bondModel.marketid = "BCF";
+        bondModel.plateform = "Bloomberg";
+        bondModel.instrumentName = "25VG - LLOYDS 2.375% 09 Apr 2026";
+        bondModel.currency = "EUR";
+        bondModel.isin = "XS2151069775";
+
+
+        return bondModel;
 
 }
 
 public Product createBondProduct(BondModel bondModel){
 
-    DateTimeFormatter formatter  = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    DateTimeFormatter formatter  = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSz");
     ZonedDateTime maturityDate = ZonedDateTime.parse(bondModel.maturityDate, formatter);
     Date terminationDate = Date.of(maturityDate.getYear(), maturityDate.getMonthValue(), maturityDate.getDayOfMonth());
+
+    ZonedDateTime zonedSettlementDate = ZonedDateTime.parse(bondModel.settlementDate, formatter);
+    Date settlementDate = Date.of(zonedSettlementDate.getYear(), zonedSettlementDate.getMonthValue(), zonedSettlementDate.getDayOfMonth());
 
         Product product = Product.builder()
                 .setTransferableProduct(TransferableProduct.builder()
@@ -70,8 +106,16 @@ public Product createBondProduct(BondModel bondModel){
                                         .setDebtType(DebtType.builder()
                                                 .setDebtClass(DebtClassEnum.VANILLA))
                                         .setIdentifier(List.of(AssetIdentifier.builder()
-                                                        .setIdentifierType(AssetIdTypeEnum.ISIN)
-                                                .setIdentifierValue(bondModel.getIsin())))))
+                                                .setIdentifierType(AssetIdTypeEnum.ISIN)
+                                                .setIdentifierValue(bondModel.getIsin())))
+                                                .addIdentifier(AssetIdentifier.builder()
+                                                    .setIdentifier(FieldWithMetaString.builder()
+                                                        .setValue(bondModel.instrumentName))
+                                                        .setIdentifierType(AssetIdTypeEnum.NAME))
+                                                .addIdentifier(AssetIdentifier.builder()
+                                                        .setIdentifier(FieldWithMetaString.builder()
+                                                                .setValue("LLOYDS"))
+                                                        .setIdentifierType(AssetIdTypeEnum.NAME))))
                         .setEconomicTerms(EconomicTerms.builder()
                                 .setTerminationDate(AdjustableOrRelativeDate.builder()
                                         .setAdjustableDate(AdjustableDate.builder()
@@ -88,7 +132,17 @@ public Product createBondProduct(BondModel bondModel){
                                                                                 .setPriceExpression(PriceExpressionEnum.PERCENTAGE_OF_NOTIONAL))
                                                                         .setPrice(ReferenceWithMetaPriceSchedule.builder()
                                                                                 .setValue(PriceSchedule.builder()
-                                                                                        .setValue(BigDecimal.valueOf(Long.parseLong(bondModel.couponRate)))))))))))))
+                                                                                        .setValue(BigDecimal.valueOf(Double.parseDouble(bondModel.couponRate)))))))))))
+                                .addPayout(Payout.builder()
+                                        .setSettlementPayout(SettlementPayout.builder()
+                                                .setPayerReceiver(PayerReceiver.builder()
+                                                        .setPayer(CounterpartyRoleEnum.PARTY_1)
+                                                        .setReceiver(CounterpartyRoleEnum.PARTY_2))
+                                                .setSettlementTerms(SettlementTerms.builder()
+                                                        .setSettlementDate(SettlementDate.builder()
+                                                                .setAdjustableOrRelativeDate(AdjustableOrAdjustedOrRelativeDate.builder()
+                                                                                .setAdjustedDate(FieldWithMetaDate.builder()
+                                                                                        .setValue(settlementDate)))))))))
 
                 .build();
 
@@ -98,9 +152,45 @@ public Product createBondProduct(BondModel bondModel){
 
 public Trade createTrade(Product bond, BondModel bondModel){
 
-    DateTimeFormatter formatter  = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    DateTimeFormatter formatter  = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSz");
     ZonedDateTime tradeDate = ZonedDateTime.parse(bondModel.tradeDate, formatter);
     Date tradeDateStr= Date.of(tradeDate.getYear(), tradeDate.getMonthValue(), tradeDate.getDayOfMonth());
+
+    Party buyerParty = Party.builder()
+                    .setPartyId(List.of(PartyIdentifier.builder()
+                            .setIdentifierValue("ClientId"))).build();
+
+
+    Party sellerParty = Party.builder()
+                    .setPartyId(List.of(PartyIdentifier.builder()
+                            .setIdentifierValue("DealerId"))).build();
+
+    Party traderParty = Party.builder()
+                    .setPartyId(List.of(PartyIdentifier.builder()
+                            .setIdentifierValue("Trader"))).build();
+
+    List<Party> partyList = List.of(buyerParty, sellerParty, traderParty);
+
+    PartyRole buyer = PartyRole.builder()
+            .setRole(PartyRoleEnum.BUYER)
+            .setPartyReference(ReferenceWithMetaParty.builder()
+                    .setValue(buyerParty))
+            .build();
+
+    PartyRole seller = PartyRole.builder()
+            .setRole(PartyRoleEnum.SELLER)
+            .setPartyReference(ReferenceWithMetaParty.builder()
+                    .setValue(sellerParty))
+                    .build();
+
+    PartyRole trader = PartyRole.builder()
+            .setRole(PartyRoleEnum.BOOKING_PARTY)
+            .setPartyReference(ReferenceWithMetaParty.builder()
+                    .setValue(traderParty))
+            .build();
+
+    List<PartyRole> partyRoleList = List.of(buyer, seller, trader);
+
 
         Trade trade = Trade.builder()
                 .setTradeDate(FieldWithMetaDate.builder()
@@ -109,10 +199,65 @@ public Trade createTrade(Product bond, BondModel bondModel){
                         .setValue(TimeZone.builder()
                                 .setLocation(FieldWithMetaString.builder()
                                         .setValue("UTC"))))
-                .setTradeLot(TradeLot.builder()
+                .setTradeIdentifier(List.of(TradeIdentifier.builder()
+                        .setIdentifierType(TradeIdentifierTypeEnum.UNIQUE_TRANSACTION_IDENTIFIER)
+                                .setAssignedIdentifier(List.of(AssignedIdentifier.builder()
+                                                .setIdentifierValue("UTI123")))))
+                .setTradeLot(List.of(TradeLot.builder()
                         .setPriceQuantity(List.of(PriceQuantity.builder()
-                                .setPrice
-                                        .setValue(BigDecimal.valueOf(Long.parseLong(bondModel.price)))))))
+                                .setPrice(List.of(FieldWithMetaPriceSchedule.builder()
+                                        .setValue(PriceSchedule.builder()
+                                                .setValue(BigDecimal.valueOf(Double.parseDouble(bondModel.price)))
+                                                .setUnit(UnitType.builder()
+                                                        .setCurrencyValue(bondModel.currency))
+                                                .setPerUnitOf(UnitType.builder()
+                                                        .setCurrencyValue(bondModel.currency))
+                                                .setPriceType(PriceTypeEnum.ASSET_PRICE)
+                                                .setComposite(PriceComposite.builder()
+                                                        .setBaseValue(BigDecimal.valueOf(Double.parseDouble(bondModel.price)))
+                                                        .setOperand(BigDecimal.valueOf(Double.parseDouble(".0213")))
+                                                        .setArithmeticOperator(ArithmeticOperationEnum.ADD)
+                                                        .setOperandType(PriceOperandEnum.ACCRUED_INTEREST)))))
+                                        .setQuantity(List.of(FieldWithMetaNonNegativeQuantitySchedule.builder()
+                                                        .setValue(NonNegativeQuantitySchedule.builder()
+                                                                .setValue(BigDecimal.valueOf(Double.parseDouble(bondModel.nominalQuantity)))
+                                                                .setUnit(UnitType.builder()
+                                                                        .setCurrencyValue(bondModel.currency)))))
+                                        .setObservable(FieldWithMetaObservable.builder()
+                                                .setValue(Observable.builder()
+                                                        .setAsset(Asset.builder()
+                                                                .setInstrument(Instrument.builder()
+                                                                        .setSecurity(Security.builder()
+                                                                                .setIdentifier(List.of(AssetIdentifier.builder()
+                                                                                        .setIdentifierType(AssetIdTypeEnum.ISIN)
+                                                                                        .setIdentifierValue(bondModel.getIsin())))
+                                                                                        .addIdentifier(AssetIdentifier.builder()
+                                                                                                .setIdentifier(FieldWithMetaString.builder()
+                                                                                                        .setValue(bondModel.instrumentName))
+                                                                                                .setIdentifierType(AssetIdTypeEnum.NAME)))))))))
+                        .addPriceQuantity(PriceQuantity.builder()
+                                .setPrice(List.of(FieldWithMetaPriceSchedule.builder()
+                                        .setValue(PriceSchedule.builder()
+                                                .setCashPrice(CashPrice.builder()
+                                                        .setCashPriceType(CashPriceTypeEnum.FEE))
+                                                .setValue(BigDecimal.valueOf(Double.parseDouble(bondModel.commission)))))))))
+
+                .setCounterparty(List.of(Counterparty.builder()
+                                .setPartyReference(ReferenceWithMetaParty.builder()
+                                        .setExternalReference("Client"))
+                                .setRole(CounterpartyRoleEnum.PARTY_1)))
+                .addCounterparty(Counterparty.builder()
+                        .setPartyReference(ReferenceWithMetaParty.builder()
+                                .setExternalReference("Dealer"))
+                        .setRole(CounterpartyRoleEnum.PARTY_2))
+                .setParty(partyList)
+                .setPartyRole(partyRoleList)
+                .setExecutionDetails(ExecutionDetails.builder()
+                        .setExecutionVenue(LegalEntity.builder()
+                                .setEntityId(List.of(FieldWithMetaString.builder()
+                                                .setValue(bondModel.marketid)))
+                                .addEntityId(FieldWithMetaString.builder()
+                                                .setValue(bondModel.plateform))))
                         .build();
 
 
